9bad80f4f25168f305229d804cd2be8d8307e3bb
laurelcerrato <lcerratoramirez@byupathway.edu>
2026-07-25 18:04:54 -0600

    fixed errors

--- week3/scripts/weather.js
+++ week3/scripts/weather.js
@@ -1,6 +1,7 @@
 const temp = document.querySelector("#current-temp");
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
+const weatherContainer = document.querySelector('.weather');
 const mylat = "49.750042270512154";
 const mylong = "6.637199289892592";
 const myApi = "45de18d04b727c7223d9fb5e769cd624";
@@ -26,11 +27,13 @@ async function apiFetch() {
 
 
 function displayResults(data) {
+    const iconImg = document.createElement("img");
     temp.innerHTML = ` ${data.main.temp}&deg;F`;
-    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     let desc = data.weather[0].description;
-    weatherIcon.setAttribute('src', iconsrc);
-    weatherIcon.setAttribute('alt', 'Current weather icon');
+    iconImg.setAttribute('src', iconsrc);
+    iconImg.setAttribute('alt', 'Current weather icon');
+    weatherContainer.appendChild(iconImg);
     captionDesc.textContent = `${desc}`;
 };
 apiFetch();
